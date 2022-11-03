d2a0b3bf226ec52fe20ebb3ded6c1178d515c96a
kobas <gorkowski.piotr1@gmail.com>
2022-11-03 23:16:13 +0100

    Revert "working scrapping"
    This reverts commit c0002f65d0c74fcb8edaec6476202fcb74c7464a.

--- Selenium-Features/vistula_scraping.py
+++ Selenium-Features/vistula_scraping.py
@@ -1,5 +1,3 @@
-import json
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -11,17 +9,27 @@ class Category:
         self.subcategories = list()
         self.link = link
 
+    def print(self):
+        print(self.main, self.category, self.link)
+        if self.subcategories:
+            for subcategory in self.subcategories:
+                print(subcategory.main, subcategory.category, subcategory.link)
+        print()
+        print()
+
 class Product:
     def __init__(self, categories, url):
+        self.code = "code"
+        self.name = "name"
+        self.price = "price"
+        self.size = "size"
+        self.discount_price = "discount_price"
+        self.photos_urls = "photos_urls"
+        self.description = "description"
         self.categories = categories
+        self.details = "details"
+        self.material = "material"
         self.url = url
-        self.code = None
-        self.name = None
-        self.price = None
-        self.without_discount_price = None
-        self.details = list()
-        self.photos_urls = list()
-        self.size = list()
 
 
 PATH = "C:\\Drive\\chromeDriver\\chromedriver.exe"
@@ -29,7 +37,9 @@ driver = webdriver.Chrome(PATH)
 
 SHOP_PATH = "https://vistula.pl/"
 SUBPAGES = ['mezczyzna', 'kobieta']
-PRODUCTS = list()
+#NUM_OF_PAGES = [18, 3]
+NUM_OF_PAGES = [1, 1]
+PRODUCT_LINKS = list()
 CATEGORIES = list()
 
 driver.get(SHOP_PATH + SUBPAGES[0])
@@ -45,14 +55,14 @@ for i in range(len(SUBPAGES)):
     for category in categories:
         try:
             certain_category = category.find_element(by=By.CSS_SELECTOR, value="a")
-            CATEGORIES.append(Category(SUBPAGES[i].upper(), certain_category.text, certain_category.get_attribute("href")))
+            CATEGORIES.append(Category(SUBPAGES[i], certain_category.text, certain_category.get_attribute("href")))
         except:
             pass
 
 for category in CATEGORIES:
     driver.get(category.link)
     subcategories = driver.find_elements(by=By.CLASS_NAME, value="sidebarChildrenCategory")
-    if subcategories and category.category != "JEANSY": #sidecase wrong website implementation
+    if subcategories:
         for subcategory in subcategories:
             try:
                 certain_subcategory = subcategory.find_element(by=By.CSS_SELECTOR, value="a")
@@ -68,7 +78,7 @@ for category in CATEGORIES:
             products = page.find_elements(by=By.CSS_SELECTOR, value="a")
             for product in products:
                 try:
-                    PRODUCTS.append(Product(subcategory, product.get_attribute("href")))
+                    PRODUCT_LINKS.append(Product(subcategory, product.get_attribute("href")))
                 except:
                     pass
     else:
@@ -77,76 +87,59 @@ for category in CATEGORIES:
         products = page.find_elements(by=By.CSS_SELECTOR, value="a")
         for product in products:
             try:
-                PRODUCTS.append(Product(category, product.get_attribute("href")))
+                PRODUCT_LINKS.append(Product(category, product.get_attribute("href")))
             except:
                 pass
 
-print(len(PRODUCTS))
-for product in PRODUCTS:
-    driver.get(product.url)
+
+# for i in range(len(SUBPAGES)):
+#     for j in range(NUM_OF_PAGES[i]):
+#         driver.get(SHOP_PATH + SUBPAGES[i] + '?page=' + str(j + 1))
+#         page = driver.find_element(by=By.CLASS_NAME, value="product-page-items")
+#         products = page.find_elements(by=By.CSS_SELECTOR, value="a")
+#         for product in products:
+#             try:
+#                 PRODUCT_LINKS.append(product.get_attribute("href"))
+#             except:
+#                 pass
+
+
+PRODUCTS = list()
+for product in PRODUCT_LINKS:
+    driver.get(product)
 
     try:
         product_code_div = driver.find_element(by=By.CLASS_NAME, value="product-code")
         product_code = product_code_div.find_element(by=By.CSS_SELECTOR, value="h2").text.replace('KOD PRODUKTU: ', '')
-        product.code = product_code
+        print(product_code)
     except:
         pass
 
     try:
         title_div = driver.find_element(by=By.CLASS_NAME, value="product-title")
         title = title_div.find_element(by=By.CSS_SELECTOR, value="h1").text
-        product.name = title
+        print(title)
     except:
         pass
 
     try:
         price_div = driver.find_element(by=By.CLASS_NAME, value="product-price")
         price = price_div.find_element(by=By.CSS_SELECTOR, value="span").text.replace(' ZŁ', '')
-        product.price = price
+        print(price)
     except:
         pass
 
-    try:
-        price_div = driver.find_element(by=By.CLASS_NAME, value="product-price")
-        price = price_div.find_element(by=By.CSS_SELECTOR, value="del").text.replace(' ZŁ', '')
-        if price:
-            product.without_discount_price = price
-    except:
-        pass
-
-    try:
-        details = driver.find_elements(by=By.CLASS_NAME, value="product-description-text")
-        for detail in details:
-            product.details.append(detail.text)
-    except:
-        pass
-
-    try:
-        photos_div = driver.find_elements(by=By.CLASS_NAME, value="open-gallery")
-        for photo_div in photos_div:
-            photo = photo_div.find_element(by=By.CSS_SELECTOR, value="img")
-            product.photos_urls.append(photo.get_attribute("src"))
-    except:
-        pass
-
-    try:
-        size_div = driver.find_element(by=By.CLASS_NAME, value="select-items")
-        sizes = size_div.find_elements(by=By.CLASS_NAME, value="noselect")
-        for size in sizes:
-            product.size.append(size.get_attribute('innerHTML').replace(' <i class="fas fa-sm fa-info-circle"></i>', ''))
-    except:
-        pass
-
-PRODUCTS_DICTIONARY = {f"product{i}": {
-                            "category": f"{PRODUCTS[i].categories.main}/{PRODUCTS[i].categories.category}",
-                            "url": PRODUCTS[i].url,
-                            "code": PRODUCTS[i].code,
-                            "name": PRODUCTS[i].name,
-                            "price": PRODUCTS[i].price,
-                            "without_discount_price": PRODUCTS[i].without_discount_price,
-                            "details": PRODUCTS[i].details,
-                            "photos_urls": PRODUCTS[i].photos_urls,
-                            "size": PRODUCTS[i].size} for i in range(len(PRODUCTS))}
-
-with open("data.json", "w", encoding="utf-8") as outfile:
-    json.dump(PRODUCTS_DICTIONARY, outfile, ensure_ascii=False, indent=2)
+    # try:
+    #     #size placeholder
+    # except:
+    #     pass
+    #
+    # try:
+    #     #discount price placeholder
+    # except:
+    #     pass
+    #
+    # try:
+    #     #photos placeholder
+    # except:
+    #     pass
