740d999945799394d17bd52a721f05a8f5e1f71f
jan-Kulpas <krzychu3813@gmail.com>
2022-11-22 22:05:26 +0100

    prestashop init

---
+++ prestashop/app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysql',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'prestashop',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'OxuagdTaoEGMgbTKrpyShK2jhrRf2ZiP9GxwhQjKtLtgLJIrAM7muQQBArA1pzrP',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2022-11-22',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'g7N6warwwzhEPTM9jHff6nVEhVngMpFNvp08viotvdeCow93007r3aDqmrCaIzOT',
+    'cookie_iv' => 'aPbjOVMeQLsaLIJ5c5V4LysQTuVRaipV',
+    'new_cookie_key' => 'def0000004e66b53f67356f627ec056472b4da1ce3e4f15b0eb4ac4fe00f33b9026b82e810117036f719fd2273f8140d5e5023965ef66cdb6eeacb22f6c655f4eb2f1d71',
+  ),
+);
