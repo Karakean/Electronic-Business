dc790b52f9d104cbf2fd395cbe45a7c1b82026cc
Karakean <mipigeno@gmail.com>
2023-04-01 16:21:21 +0200

    deployment

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
+FROM alpine:3.17 as source
+
+RUN apk update
+RUN apk add git
+RUN git clone -b master https://github.com/Karakean/Electronic-Business.git
+
+
 FROM prestashop/prestashop:8.0.0
+
+RUN rm -rf .
+COPY --from=source /Electronic-Business/prestashop .
 
 RUN apt update -y
 RUN apt install -y memcached
@@ -6,15 +16,14 @@ RUN apt install -y libz-dev libmemcached-dev
 RUN pecl install memcached
 RUN echo extension=memcached.so >> /usr/local/etc/php/conf.d/memcached.ini
 
-RUN rm -r /var/www/html/*
-COPY prestashop /var/www/html
-
-COPY ssl /etc/ssl/dlugierozdzki
-COPY default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+COPY --from=source /Electronic-Business/ssl /etc/ssl/dlugierozdzki
+COPY --from=source /Electronic-Business/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 RUN ln -s /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/default-ssl.conf
 RUN a2enmod ssl
 
-RUN chmod -R 777 /var/www/html/var
-#RUN chmod -R 777 /var/www/html/install
-#RUN rm -rf /var/www/html/install
+RUN chmod -R 755 .
+RUN chown -R www-data:www-data .
+RUN rm -rf var/cache/prod/*
+
 EXPOSE 80
+EXPOSE 443
--- create_database.sh
+++ create_database.sh
@@ -6,9 +6,10 @@ DB_PASSWORD="password"
 DB_ROOT_PASSWORD="root"
 DB_ROOT_LOGIN="root"
 DUMP_FILE="./dbinit/dlugierozdzkidb.sql"
+CONTAINER_NAME="be_184865_db"
 
-docker exec -it be_184865_db mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-docker exec -it be_184865_db mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${DB_USER}@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-docker exec -it be_184865_db mysql -p$DB_ROOT_PASSWORD  -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-docker exec -it be_184865_db mysql -p$DB_ROOT_PASSWORD  -e "FLUSH PRIVILEGES;"
-docker exec -i be_184865_db mysql -p$DB_ROOT_PASSWORD $DB_NAME < $DUMP_FILE
+docker exec -it $CONTAINER_NAME mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+docker exec -it $CONTAINER_NAME mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${DB_USER}@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+docker exec -it $CONTAINER_NAME mysql -p$DB_ROOT_PASSWORD  -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+docker exec -it $CONTAINER_NAME mysql -p$DB_ROOT_PASSWORD  -e "FLUSH PRIVILEGES;"
+docker exec -i $CONTAINER_NAME mysql -p$DB_ROOT_PASSWORD $DB_NAME < $DUMP_FILE
